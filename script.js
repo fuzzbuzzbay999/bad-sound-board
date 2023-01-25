21a289e135c9780c1f73e7964bf738c92629a7ac
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2023-01-24 23:48:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function jazzloaded(){
+var volume=document.getElementById("volume");
+var speed=document.getElementById("playspeed");
+var audio=document.getElementById("jazz");
+var speedOut = document.getElementById("speedOut");
+var volumeOut = document.getElementById("volumeOut");
+console.log("ples work");
+console.log(audio.volume);
+console.log(audio.playbackRate);
+
+console.log(volume.value);
+console.log(speed.value);
+
+audio.volume=volume.value*0.01;
+audio.playbackRate=speed.value;
+volumeOut.textContent=volume.value+"%";
+speedOut.textContent=speed.value+"X";
+}
