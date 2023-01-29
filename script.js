e15f355fba40dccced4e7a46b7a65b3b5b16882c
fuzzbuzzbay999 <51093507+fuzzbuzzbay999@users.noreply.github.com>
2023-01-29 14:25:37 -0700

    fin!!! maybe

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function jazzloaded(){
 var volume=document.getElementById("volume");
 var speed=document.getElementById("playspeed");
-var audio=document.getElementById("jazz");
+var audio=document.getElementById("music");
 var speedOut = document.getElementById("speedOut");
 var volumeOut = document.getElementById("volumeOut");
 console.log("ples work");
@@ -15,4 +15,11 @@ audio.volume=volume.value*0.01;
 audio.playbackRate=speed.value;
 volumeOut.textContent=volume.value+"%";
 speedOut.textContent=speed.value+"X";
+
+var audio=document.getElementById("music2");
+audio.volume=volume.value*0.01;
+audio.playbackRate=speed.value;
+volumeOut.textContent=volume.value+"%";
+speedOut.textContent=speed.value+"X";
+
 }
